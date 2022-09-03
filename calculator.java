f3b39b98284e5c1e987d9489bb4637ed9ee21cbe
surajlangute <surajlangute55@gmail.com>
2022-09-03 14:57:43 +0530

    added subtraction files

--- calculator.java
+++ calculator.java
@@ -6,9 +6,13 @@ public class calculator {
 	{
 		int a =10;
 		int b =10;
+		
 		int sum = a+b;
+		int sub = a-b;
+		
 		System.out.println("welcome to calculator");
 		System.out.println("sum is :"+sum);
+		System.out.println("subtraction  is :" +sub);
 	}
 
 }
