a67ea779f1984bf78bec14a5e3ea864a056cfaef
surajlangute <surajlangute55@gmail.com>
2022-09-03 15:04:55 +0530

    added division files

--- calculator.java
+++ calculator.java
@@ -10,11 +10,13 @@ public class calculator {
 		int sum = a+b;
 		int sub = a-b;
 		int mul = a*b;
+		int div = a/b;
 		
 		System.out.println("welcome to calculator");
 		System.out.println("sum is :"+sum);
 		System.out.println("subtraction  is :" +sub);
 		System.out.println("multiplication  is :" +mul);
+		System.out.println("division  is :" +div);
 	}
 
 }
